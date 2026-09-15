fe9c1b94a8bfc5b0dc3940189e2fd501b97e8e71
Pierre <pierre@dev.local>
2026-09-15 13:21:20 +0000

    fix: add missing using for Microsoft.Extensions.Configuration

--- tests/HomelabDashboard.Tests/AuthIntegrationTests.cs
+++ tests/HomelabDashboard.Tests/AuthIntegrationTests.cs
@@ -7,6 +7,7 @@ using HomelabDashboard.Models;
 using HomelabDashboard.Services;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
